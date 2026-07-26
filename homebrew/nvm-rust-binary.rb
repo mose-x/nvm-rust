f2c93498fc14f08dbbf8a72ae93659c956148e26
x-zm <602187256@qq.com>
2026-07-26 16:19:49 +0800

    fix: fill homebrew binary formula sha256 for v2.0.0 (#17)

--- homebrew/nvm-rust-binary.rb
+++ homebrew/nvm-rust-binary.rb
@@ -7,20 +7,20 @@ class NvmRust < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/mose-x/nvm-rust/releases/download/v2.0.0/nvm-2.0.0-macos-x64.tar.gz"
-      sha256 "TO_BE_FILLED"
+      sha256 "8af3dc4a4d9391cfb849dea2f78b64850b538a085f79e41906638c22b13c3cb3"
     elsif Hardware::CPU.arm?
       url "https://github.com/mose-x/nvm-rust/releases/download/v2.0.0/nvm-2.0.0-macos-arm64.tar.gz"
-      sha256 "TO_BE_FILLED"
+      sha256 "f8da8705f1ccb21e14bb6f0f9168df654786a50909798a8e02f44bde173cf756"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/mose-x/nvm-rust/releases/download/v2.0.0/nvm-2.0.0-linux-x64.tar.gz"
-      sha256 "TO_BE_FILLED"
+      sha256 "a10055b2940600c406a28897b63fdbd56644c5a86f26fa6bb6dabf02a35154a7"
     elsif Hardware::CPU.arm?
       url "https://github.com/mose-x/nvm-rust/releases/download/v2.0.0/nvm-2.0.0-linux-arm64.tar.gz"
-      sha256 "TO_BE_FILLED"
+      sha256 "7a17351b58937a5801934250c5b85bd056407893d0939e194bfe0ec90b6d9465"
     end
   end
 
